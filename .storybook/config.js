b8d9442118508e6982faa7de749713354f3e1cab
Šimon Mareš <simon.mares@protonmail.com>
2018-08-13 10:03:33 +0200

    tool: Remove custom storybook configs

--- .storybook/config.js
+++
@@ -1,8 +0,0 @@
-import { configure } from '@storybook/react';
-import '@storybook/addon-actions/register';
-
-function loadStories() {
-  require('./stories.js');
-}
-
-configure(loadStories, module);
