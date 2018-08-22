82b45231bc517aed1b1d9d0f6c8acc3b588cab3a
Šimon Mareš <simon.mares@protonmail.com>
2018-08-22 14:57:55 +0200

    chore: Update env-setup.js to remove old code

--- jest/env-setup.js
+++ jest/env-setup.js
@@ -2,12 +2,4 @@
 const Enzyme = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
 
-// See TUTO_IMPL_DEV_GLOBAL
-global.__DEV__ = true;
-
-// localStorage global stub (used in reducers, should not be)
-global.localStorage = {
-  getItem() {},
-};
-
 Enzyme.configure({ adapter: new Adapter() });
