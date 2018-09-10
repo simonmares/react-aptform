493fad1aa8570fc30b4eaf83f8b1057de5c456ff
Šimon Mareš <simon.mares@protonmail.com>
2018-09-10 12:53:49 +0200

    tests: Add test file for experimental features bc of TDD

---
+++ tests/experimental-api.test.js
@@ -0,0 +1,51 @@
+// @flow
+
+import * as React from 'react';
+import { render, waitForElement, cleanup } from 'react-testing-library';
+
+import { Aptform } from '../src/index';
+
+import { defaultProps, toStableJSON as toJSON, changeInputValue } from './helpers';
+
+describe('synchronizing', () => {
+  const inputs = { email: { required: true } };
+  const initialValues = { email: 'InitialValue' };
+
+  afterEach(() => {
+    cleanup();
+  });
+
+  test('onChange', async () => {
+    const onChange = jest.fn();
+
+    let form;
+    let renderCount = 0;
+    const { container, getByText } = render(
+      <Aptform
+        {...defaultProps}
+        inputs={inputs}
+        initialValues={initialValues}
+        onChange={onChange}
+        render={renderProps => {
+          renderCount += 1;
+          form = renderProps.form;
+          const { email } = renderProps.inputs;
+          return toJSON({ value: email.value, renderCount });
+        }}
+      />
+    );
+
+    expect(container.textContent).toBe(toJSON({ value: 'InitialValue', renderCount: 1 }));
+
+    changeInputValue(form, 'email', 'UpdatedValue');
+
+    // rerenders:
+    // 1) initial
+    // 2) change
+    // 3) on delay => reset validity
+    // 4) validation => set validity
+    // 5) on type timeout => not typing
+    await waitForElement(() => getByText(toJSON({ value: 'UpdatedValue', renderCount: 5 })));
+    expect(onChange).toHaveBeenCalledWith('email', 'UpdatedValue');
+  });
+});
