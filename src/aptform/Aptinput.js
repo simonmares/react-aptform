2ec99c535ea6465eef8294e23ef46e666d7a239d
Šimon Mareš <simon.mares@protonmail.com>
2019-06-26 10:28:28 +0200

    feat(input): Add state initialization

--- src/aptform/Aptinput.js
+++ src/aptform/Aptinput.js
@@ -1,11 +1,43 @@
 // @flow
 
-type AptinputProps = {|
-  name: string,
-|};
+//
+// Helpers
+//
+
+function nonNil<T>(val: ?T, defaultVal: T): T {
+  return val !== undefined && val !== null ? val : defaultVal;
+}
+
+//
+// Constants
+//
+
+const defaultValue = '';
+
+//
+// Aptinput implementation
+//
 
 type AptinputState = {|
-  value: string,
+  value: any,
+  // input is currently valid or not (or its unknown)
+  valid: boolean | typeof undefined,
+  // input has been focused
+  touch: boolean,
+  // input has now focus
+  focus: boolean,
+  // input has changed from initial value (provided or our default)
+  pristine: boolean,
+  // user still types (from heuretics)
+  changing: boolean,
+  // just error text
+  error: string,
+|};
+
+type AptinputProps = {|
+  name: string,
+  required?: boolean,
+  initialState?: $Shape<AptinputState>,
 |};
 
 export class Aptinput {
@@ -13,6 +45,35 @@ export class Aptinput {
   state: AptinputState;
 
   constructor(props: AptinputProps) {
-    this.state = { value: '' };
+    const initialState = props.initialState || {};
+
+    this.state = {
+      value: nonNil(initialState.value, defaultValue),
+      valid: initialState.valid,
+      touch: nonNil(initialState.touch, false),
+      focus: nonNil(initialState.focus, false),
+      pristine: nonNil(initialState.pristine, true),
+      changing: nonNil(initialState.changing, false),
+      error: nonNil(initialState.error, ''),
+    };
   }
+
+  //
+  // Public API
+  //
+
+  // error | success
+  show() {}
+
+  // error | changed
+  has() {}
+
+  getPassProps() {}
+
+  // valid | pristine | validating
+  is() {}
+
+  //
+  // Private helpers
+  //
 }
--- src/aptform/Aptinput.test.js
+++ src/aptform/Aptinput.test.js
@@ -3,8 +3,8 @@
 import { Aptinput } from './Aptinput';
 
 describe('Aptinput initiate', () => {
-  const createUnit = () => {
-    return new Aptinput({ name: 'email' });
+  const createUnit = (props) => {
+    return new Aptinput({ name: 'email', ...props });
   };
 
   test('can be initiated', () => {
@@ -14,6 +14,32 @@ describe('Aptinput initiate', () => {
 
   test('has initial empty state', () => {
     const unit = createUnit();
-    expect(unit.state).toEqual({ value: '' });
+    expect(unit.state).toEqual({
+      value: '',
+      valid: undefined,
+      touch: false,
+      focus: false,
+      pristine: true,
+      changing: false,
+      error: '',
+    });
+  });
+
+  test('set initial state', () => {
+    const initialState = {
+      value: 'tom@example',
+      valid: false,
+      touch: true,
+      focus: true,
+      pristine: false,
+      changing: true,
+      error: 'This is not a valid e-mail.',
+    };
+
+    const unit = createUnit({
+      initialState,
+    });
+
+    expect(unit.state).toEqual(initialState);
   });
 });
