47773e4b4b67917cb96355b8b6250f256258374f
Šimon Mareš <simon.mares@protonmail.com>
2019-07-03 11:53:28 +0200

    docs: Add some styles for form elements

--- examples/versionTwo.example.js
+++ examples/versionTwo.example.js
@@ -1,8 +1,11 @@
 // @flow
 
 import * as React from 'react';
+import { useRef } from 'react';
 
 import { useForm } from '../src/react-aptform';
+
+import './form-semantic.css';
 
 const staticProps = {
   inputs: {
@@ -12,17 +15,44 @@ const staticProps = {
 };
 
 function BasicExample() {
-  const { inputs, form, aptform } = useForm(staticProps);
+  const submitValues = useRef({});
+
+  const onSubmit = (values) => {
+    submitValues.current = values;
+    console.log('values', values);
+    form.reset();
+    return Promise.resolve(true);
+  };
+
+  const { inputs, form, aptform } = useForm({ ...staticProps, onSubmit });
   const { name, surname } = inputs;
+
+  const formInvalid = !form.is('valid');
+
+  const renderForm = () => {
+    return (
+      <form {...form.getPassProps()}>
+        <fieldset>
+          <label htmlFor="name_id">Name</label>
+          <input type="text" id="name_id" {...name.getPassProps()} />
+        </fieldset>
+        <fieldset>
+          <label htmlFor="surname_id">Surname</label>
+          <input type="text" id="surname_id" {...surname.getPassProps()} />
+        </fieldset>
+        <button type="submit" disable={formInvalid}>
+          Submit
+        </button>
+      </form>
+    );
+  };
+
   return (
-    <form {...form.getPassProps()}>
-      <div>
-        Name: <input type="text" {...name.getPassProps()} />
-      </div>
-      <div>
-        Surname: <input type="text" {...surname.getPassProps()} />
-      </div>
-    </form>
+    <div>
+      {renderForm()}
+      <hr />
+      submit values: {JSON.stringify(submitValues.current, null, 2)}
+    </div>
   );
 }
 // <button type="submit" disabled={form.submitting}>
@@ -31,6 +61,24 @@ function BasicExample() {
 // <span>{form.isValid() ? '' : 'Please fill all fields'}</span>
 // {form.submitFailed ? form.submitErrorText : ''}
 
+function StyleExample() {
+  return (
+    <form>
+      <fieldset>
+        <label htmlFor="name_id">Name</label>
+        <input type="text" id="name_id" value="..." />
+      </fieldset>
+      <fieldset>
+        <label htmlFor="surname_id">Surname</label>
+        <input type="text" id="surname_id" value="..." />
+      </fieldset>
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
 export default {
   basic: () => <BasicExample />,
+  style: () => <StyleExample />,
 };
